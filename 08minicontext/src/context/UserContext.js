142f9e6e3398033d10416855b9d1df408592ccbf
Aditya Kaushik <aditya.kaushik2505@gmail.com>
2024-06-14 22:22:11 +0530

    Context api

---
+++ 08minicontext/src/context/UserContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+/*
+ *Every Context is a provider
+ *With every context we have to create a provider
+ */
+const UserContext = React.createContext();
+
+export default UserContext;
